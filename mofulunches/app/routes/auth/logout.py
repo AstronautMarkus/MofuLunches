6b61ec9c2cce1238be24e3abb3ffa86700593db5
Marcos Reyes <astronautmarkus@gmail.com>
2025-11-26 22:38:02 -0300

    fix: se corrige el método de logout a POST.

--- mofulunches/app/routes/auth/logout.py
+++ mofulunches/app/routes/auth/logout.py
@@ -2,7 +2,7 @@ from . import auth
 from flask import redirect, url_for, flash
 from flask_login import logout_user, login_required
 
-@auth.route('/logout')
+@auth.route('/logout', methods=['POST'])
 @login_required
 def logout():
     logout_user()
